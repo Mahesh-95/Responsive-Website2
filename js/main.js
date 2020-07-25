74b4f055cf8369d680b9405f1876df0d3995d52e
Mahesh-95 <53552767+Mahesh-95@users.noreply.github.com>
2020-07-25 19:05:00 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,36 @@
+
+var menuBar=document.getElementById("menu-bar");
+menuBar.addEventListener("click", function(){
+    var menu=document.querySelector(".menu");
+    if(menu.style.display=="none"){
+      menu.style.display="block";
+    }else{
+      menu.style.display="none";
+    }
+})
+
+
+var loader;
+
+function loadNow(opacity){
+    if(opacity<=0){
+        displayContent();
+    }
+    else{
+        loader.style.opacity=opacity;
+        window.setTimeout(function(){
+            loadNow(opacity - 0.01)
+        },10);
+    }
+}
+
+function displayContent(){
+    loader.style.display="none";
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    loader=document.getElementById("loader");
+    loadNow(1);
+});
+
+
